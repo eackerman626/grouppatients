997aefe25f75d79f357e6e59e5acb14d7b1bad16
Emily Ackerman <emily.ackerman626@gmail.com>
2023-02-04 12:01:17 -0600

    fixes get_group_patients filter

--- backend/app/data_access/repository.py
+++ backend/app/data_access/repository.py
@@ -101,11 +101,9 @@ def create_group(db: Session, group: schemas.GroupBase):
 
     
 def get_group_patients(db: Session, group_id: str | int):
-    group = db.query(models.Group).filter(
-        models.Group.id == group_id).first()
-    if group is None:
-        return None
-    return group.patients
+    patients = db.query(models.Patient).filter(
+        models.Patient.group_id == group_id).all()
+    return patients
 
 
 def set_patient_group(db: Session, patient_id: int, group_id: int):
