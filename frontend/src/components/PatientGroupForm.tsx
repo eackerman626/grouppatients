830edc17f4739f7a59f24428b6e7c5323e5e85ae
Emily Ackerman <emily.ackerman626@gmail.com>
2023-02-05 22:10:59 -0600

    filter patients in dropdown using availability checksums

--- frontend/src/components/PatientGroupForm.tsx
+++ frontend/src/components/PatientGroupForm.tsx
@@ -1,9 +1,11 @@
 import { FC, FormEvent, ChangeEvent, useState, useEffect } from 'react';
 import { PatientData, putPatientGroup, getUnassignedPatients } from '../requests/patients';
 import { GroupData } from '../requests/groups';
+import { ScheduleBlock } from './Schedule';
 
 interface AssignPatientToGroupProps {
 	group: GroupData;
+	groupPatients: PatientData[];
 	onAssignPatientToGroup: (patient: PatientData) => void;
 }
 
@@ -16,28 +18,45 @@ const defaultPatient = {
 
 const PatientGroupForm: FC<AssignPatientToGroupProps> = (props) => {
 	const [patient, setPatient] = useState<PatientData | undefined>(defaultPatient);
-	const [unassignedPatients, setUnassignedPatients] = useState<PatientData[]>([]);
+	const [availablePatients, setAvailablePatients] = useState<PatientData[]>([]);
 
 	useEffect(() => {
 		(async () => {
-			setUnassignedPatients(await getUnassignedPatients());
+			const unassignedPatients = await getUnassignedPatients();
+			setAvailablePatients(unassignedPatients.filter((pat) => doesPatientHaveOverlap(pat, props.groupPatients)));
 		})();
-	}, []);
+	}, [props.groupPatients]);
 
 	const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
 		event.preventDefault();
-		// TO-DO: figure out why setState isn't refreshing the dropdown in the UI
 		if (patient && patient.id > 0) {
 			await putPatientGroup(patient.id, props.group.id);
 			await props.onAssignPatientToGroup(patient);
 			setPatient(defaultPatient);
 		}
-		// TO-DO: refresh the list w/o the assigned patient
 	};
+
+	function doesPatientHaveOverlap(patient: PatientData, existingPatients: PatientData[]) {
+		const patCheckSumAry = checkSumAry(patient.availabilities);
+		const allAvailabilities = [];
+		if (!existingPatients.length) {
+			allAvailabilities.push([]);
+		} else {
+			for (let pat of existingPatients) {
+				allAvailabilities.push(checkSumAry(pat.availabilities));
+			}
+		}
+		const overlappingAvailabilities = allAvailabilities.reduce((accum, curr) => accum.filter((el) => curr.includes(el)), patCheckSumAry);
+		return overlappingAvailabilities.length >= 3;
+	}
+
+	function checkSumAry(availabilities: ScheduleBlock[]) {
+		return availabilities.map((availability) => availability.day_of_week + availability.start_time + availability.end_time);
+	}
 
 	const handleChange = () => (event: ChangeEvent<HTMLSelectElement>) => {
 		if (parseInt(event.target.value) > 0) {
-			const patient = unassignedPatients.find((pat) => String(pat.id) === event.target.value);
+			const patient = availablePatients.find((pat) => String(pat.id) === event.target.value);
 			setPatient(patient);
 		}
 	};
@@ -49,7 +68,7 @@ const PatientGroupForm: FC<AssignPatientToGroupProps> = (props) => {
 				<label>Patient </label>
 				<select name="Patient" id="patient" onChange={handleChange()}>
 					<option value="" />
-					{unassignedPatients.map((patient) => (
+					{availablePatients.map((patient) => (
 						<option value={patient.id} key={patient.id}>
 							{patient.first_name} {patient.last_name}
 						</option>
